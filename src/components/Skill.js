5bebc162abaf1ca4c0e29ae3342d665cde22d2a2
walter <walterceliz93@gmail.com>
2020-02-10 22:17:54 -0300

    modified Level

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 
 export default class Skills extends Component{
 
+getStringLevel(level){
+    return level ===1 ? "principiante" : level ===2 ? "intermedio" : "avanzado";
+
+}
+
 render(){
 
     const{ name, level}= this.props.info;
+    const stringLevel = this.getStringLevel(level);
 return(
 
 <div>
     <h3>{name}</h3>
-    <p>{level}</p>
+    <p>{stringLevel}</p>
 </div>
 
 
